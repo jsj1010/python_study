43e368c7fb0567f3678646e9aa7da56444101abc
jsj <jo@example.com>
2026-09-23 16:11:14 +0900

    flask

---
+++ pro9flask/fpro5getpost/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request       
+
+app = Flask(__name__);
+
+@app.route("/")
+def index():
+    return render_template("index.html");
+
+@app.route("/get_form")
+def get_form():
+    return render_template("get_form.html");
+
+@app.route("/get_result")
+def get_result():
+    name = request.args.get("username") # get 방식은 요청자료 받기
+    age = request.args.get("age")  # '23' 문자타입으로만 받기
+    age = age + "살";
+    return render_template("get_result.html", name=name, age=age)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000);
